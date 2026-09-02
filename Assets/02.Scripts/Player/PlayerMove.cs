4227adf73934bce7786862c7b78d8096381cace3
rototoma <67163388+rototoma@users.noreply.github.com>
2026-09-02 12:44:57 +0900

    feat: y좌표의 제한

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -1,45 +1,32 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // 목적: 키보드 입력에 따라서 플레이어 이동 처리를 하고 싶다.
- 
     // 필요 필드:
     public float Speed;
+    public float yMin;
     
-    // 매 프레임마다 실행된다.
-    // 초당 프레임 실행 횟수는: 별다른 설정이 없을 경우 가능한 많이
     private void Update()
     {
         // 1. 키보드 입력을 받는다.
         float h = Input.GetAxis("Horizontal");  // 키보드 왼.오른쪽 입력 상태에 따라 -1f~0~1f
         float v = Input.GetAxis("Vertical");    // 키보드 위.아래 입력 상태에 따라 -1f~0~1f
         
-        // float h = Input.GetAxisRaw("Horizontal");  // 키보드 왼.오른쪽 입력 상태에 따라 -1 0 1
-        // float v = Input.GetAxisRaw("Vertical");    // 키보드 위.아래 입력 상태에 따라 -1 0 1
-        
         Vector2 direction = new Vector2(h, v);
-
+        if (transform.position.y >= 0 && direction.y>=0)
+        {
+            direction.y = 0;
+        }
+        if (transform.position.y < yMin && direction.y<=0)
+        {
+            direction.y = 0;
+        }
+        
         Vector2 normalizedSpeed = (direction * Speed).normalized;
         
         // 3. 방향과 속도에 따라 이동한다.
-        transform.Translate(direction * Speed * Time.deltaTime);
+        transform.Translate(normalizedSpeed * Time.deltaTime);
         // == transform.position = transform.position + (Vector3)direction * Speed * Time.deltaTime;
-        // 새로운 위치 = 현재 위치 + 속도*시간
-       
-
-        // // 1. 키보드 입력을 받는다.
-        // if (Input.GetKey(KeyCode.LeftArrow))
-        // {
-        //     Debug.Log("왼쪽 방향기를 누르는 중");
-        //     // 2. 키보드 입력에 따라 방향을 구한다.
-        //     // 게임에는 벡터라는 타입이 있다. 벡터는 크기와 방향을 의미한다.
-        //     Vector2 direction = new Vector2(-1, 0);
-        //     // == Vector2 direction = Vector2.left;
-        //
-        //     // 3. 방향과 속도에 따라 이동한다.
-        //     transform.Translate(direction * Speed * Time.deltaTime);
-        //     // deltaTime: 이전 프레임부터 지금 프레임까지 시간이 얼마나 지났는지 ms 단위로 반환
-        // }
     }
 }
