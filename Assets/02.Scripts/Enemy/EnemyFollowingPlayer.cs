c7122fa3db6fc4eb449525cee566e24dc94d0b72
rototoma <67163388+rototoma@users.noreply.github.com>
2026-09-08 10:19:17 +0900

    feat: 적 헤드 방향 돌아가게

--- Assets/02.Scripts/Enemy/EnemyFollowingPlayer.cs
+++ Assets/02.Scripts/Enemy/EnemyFollowingPlayer.cs
@@ -7,6 +7,8 @@ public class EnemyFollowingPlayer : Enemy
         Vector3 myPos = transform.position;
         Vector3 playerPos = PlayerObj.transform.position;
         Vector2 direction = new Vector2(playerPos.x - myPos.x, playerPos.y - myPos.y).normalized;
+        transform.rotation =
+            Quaternion.Euler(0, 0, 180 - Mathf.Atan2(playerPos.x - myPos.x, playerPos.y - myPos.y) * Mathf.Rad2Deg);
         transform.Translate(direction * MoveSpeed * Time.deltaTime);
     }
 }
--- Assets/02.Scripts/Enemy/EnemyToPlayer.cs
+++ Assets/02.Scripts/Enemy/EnemyToPlayer.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class EnemyToPlayer : Enemy
@@ -6,8 +7,8 @@ public class EnemyToPlayer : Enemy
     {
         Vector3 myPos = transform.position;
         Vector2 direction = new Vector2(initialPlayerPos.x - myPos.x, initialPlayerPos.y - myPos.y).normalized;
-        transform.rotation = Quaternion.LookRotation(direction);
-
+        transform.rotation =
+            Quaternion.Euler(0, 0, 180 - Mathf.Atan2(initialPlayerPos.x, initialPlayerPos.y - 6) * Mathf.Rad2Deg);
         transform.Translate(direction * MoveSpeed * Time.deltaTime);
     }
 }
