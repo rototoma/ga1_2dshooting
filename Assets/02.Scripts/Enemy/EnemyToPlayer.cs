beb6054f0e6ecd23e41ce6b70627d8705ed246f3
rototoma <67163388+rototoma@users.noreply.github.com>
2026-09-08 15:52:01 +0900

    feat: Destroy존 추가

--- Assets/02.Scripts/Enemy/EnemyToPlayer.cs
+++ Assets/02.Scripts/Enemy/EnemyToPlayer.cs
@@ -5,7 +5,8 @@ public class EnemyToPlayer : Enemy
     public override void Move()
     {
         Vector3 myPos = transform.position;
-        Vector2 direction = new Vector2(initialPlayerPos.x - myPos.x, initialPlayerPos.y - myPos.y).normalized;
+        Vector2 direction = new Vector2(initialPlayerPos.x * 1.3f - myPos.x, initialPlayerPos.y * 1.3f - myPos.y)
+            .normalized;
         transform.Translate(direction * MoveSpeed * Time.deltaTime);
     }
 }
