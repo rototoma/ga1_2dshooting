7c2da650047448c84d0b6d8d9396d564804165a9
rototoma <67163388+rototoma@users.noreply.github.com>
2026-09-10 09:44:30 +0900

    fix: EnemyToPlayer 강사님 코드 쎄비지

--- Assets/02.Scripts/Enemy/EnemyToPlayer.cs
+++ Assets/02.Scripts/Enemy/EnemyToPlayer.cs
@@ -2,11 +2,29 @@ using UnityEngine;
 
 public class EnemyToPlayer : Enemy
 {
+    private Vector2 _direction;
+
+    private void Start()
+    {
+        _direction = initialPlayerPos - transform.position;
+
+        float dx = _direction.x; // 플레이어와 에너미 사이의 밑변 길이
+        float dy = _direction.y; // 플레이어와 에너미 사이의 높이 길이
+        // tan세타 = dy / dx
+        // tan^ * tan세타 = tan^ * dy / dx
+        // 세타 = tant^ * dy / dx
+        // 각도 = 세타 * Rad2Deg
+        float radian = Mathf.Atan2(dy, dx);
+        float angle = radian * Mathf.Rad2Deg + 90;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        _direction.Normalize();
+    }
+
     public override void Move()
     {
         Vector3 myPos = transform.position;
-        Vector2 direction = new Vector2(initialPlayerPos.x * 1.3f - myPos.x, initialPlayerPos.y * 1.3f - myPos.y)
-            .normalized;
-        transform.Translate(direction * MoveSpeed * Time.deltaTime);
+        // transform.Translate(direction * MoveSpeed * Time.deltaTime);
+        transform.position += (Vector3)(_direction * MoveSpeed) * Time.deltaTime;
     }
 }
