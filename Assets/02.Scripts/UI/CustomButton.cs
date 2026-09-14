dfdf3339d0d6f129929f25a7239ce7c5fc913fe6
rototoma <67163388+rototoma@users.noreply.github.com>
2026-09-14 16:35:42 +0900

    refactor: customButton
    awake를 오버라이드하여 타겟 그래픽 Image 할당 및 null여부 확인

--- Assets/02.Scripts/UI/CustomButton.cs
+++ Assets/02.Scripts/UI/CustomButton.cs
@@ -7,21 +7,27 @@ public class CustomButton : Button
     [SerializeField] private AudioSource _clickSound;
     [SerializeField] private Sprite _onSprite;
     [SerializeField] private Sprite _offSprite;
-    private Image _buttonImage => targetGraphic as Image;
+    private Image _buttonImage;
 
     [SerializeField] private bool _isInitialOn;
 
     //todo: 버튼의 종류 (토글, 연속클릭, 애니메이션 적용 등)에 따라 타입별로 사용하기
 
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _buttonImage = targetGraphic as Image;
+        if (_buttonImage == null)
+        {
+            Debug.LogError($"{name}: Target Graphic이 Image가 아닙니다.", this);
+        }
+    }
+
     public override void OnPointerClick(PointerEventData eventData)
     {
         base.OnPointerClick(eventData);
         _clickSound.Play();
-
-        if (_buttonImage == null)
-        {
-            Debug.LogError($"Button {name} has no button image");
-        }
 
         if (_onSprite != null)
         {
